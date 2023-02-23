7aa3b35254295de5bef9ef4f49f0630462390713
DESKTOP-CNS8AT7\meomeo <maisonsmd@gmail.com>
2023-02-23 23:30:03 +0700

    Align comments

--- include/groupControl/groupControl_define.h
+++ include/groupControl/groupControl_define.h
@@ -6,25 +6,25 @@
 #ifndef GROUPLIBCONTROL_DEFINE_H
 #define GROUPLIBCONTROL_DEFINE_H
 
-#define GC_SCREEN_SIZE              QSize(312, 402) //画面サイズ縦
+#define GC_SCREEN_SIZE QSize(312, 402) //画面サイズ縦
 
-#define GC_TITLE_GEOMETRY           QRect(0, 0, 312, 32) //タイトル ジオメトリ
-#define GC_FIRST_BUTTON_GEOMETRY    QRect(0, 32, 78, 48) //最初のボタンの形状
-#define GC_HISTORY_GEOMETRY         QRect(0, 272, 78, 48) //履歴ボタンの形状
-#define GC_UP_GEOMETRY              QRect(78, 272, 78, 48) //上ボタンの形状
+#define GC_TITLE_GEOMETRY           QRect(0, 0, 312, 32)    //タイトル ジオメトリ
+#define GC_FIRST_BUTTON_GEOMETRY    QRect(0, 32, 78, 48)    //最初のボタンの形状
+#define GC_HISTORY_GEOMETRY         QRect(0, 272, 78, 48)   //履歴ボタンの形状
+#define GC_UP_GEOMETRY              QRect(78, 272, 78, 48)  //上ボタンの形状
 #define GC_DOWN_GEOMETRY            QRect(156, 272, 78, 48) //下ボタンの形状
 #define GC_RETURN_GEOMETRY          QRect(234, 272, 78, 48) //戻るボタンの形状
-#define GC_SETTING_GEOMETRY         QRect(0, 320, 312, 32) //ラベル ジオメトリの設定
-#define GC_TITLE_BUTTON_GEOMETRY    QRect(0, 354, 78, 48) //タイトルボタンの形状
-#define GC_EMPTY_GEOMETRY           QRect(78, 354, 78, 48) //空のボタン ジオメトリ
+#define GC_SETTING_GEOMETRY         QRect(0, 320, 312, 32)  //ラベル ジオメトリの設定
+#define GC_TITLE_BUTTON_GEOMETRY    QRect(0, 354, 78, 48)   //タイトルボタンの形状
+#define GC_EMPTY_GEOMETRY           QRect(78, 354, 78, 48)  //空のボタン ジオメトリ
 #define GC_REGISTER_BUTTON_GEOMETRY QRect(156, 354, 78, 48) //ボタンの形状を登録する
 #define GC_DELETE_BUTTON_GEOMETRY   QRect(234, 354, 78, 48) //ボタンのジオメトリを削除
 
-#define GC_HORIZON_SCREEN_SIZE           QSize(858, 276) //画面サイズの地平線
+#define GC_HORIZON_SCREEN_SIZE      QSize(858, 276)         //画面サイズの地平線
 
-#define GC_HORIZON_TITLE_GEOMETRY        QRect(78, 2, 780, 32) //タイトル 幾何学の地平線
-#define GC_HORIZON_FIRST_BUTTON_GEOMETRY QRect(78, 34, 78, 48) //最初のボタン ジオメトリの地平線
-#define GC_HORIZON_HISTORY_GEOMETRY      QRect(78, 226, 78, 48) //履歴ボタンの形状
+#define GC_HORIZON_TITLE_GEOMETRY        QRect(78, 2, 780, 32)   //タイトル 幾何学の地平線
+#define GC_HORIZON_FIRST_BUTTON_GEOMETRY QRect(78, 34, 78, 48)   //最初のボタン ジオメトリの地平線
+#define GC_HORIZON_HISTORY_GEOMETRY      QRect(78, 226, 78, 48)  //履歴ボタンの形状
 #define GC_HORIZON_DOWN_GEOMETRY         QRect(156, 226, 78, 48) //下ボタンの形状
 #define GC_HORIZON_UP_GEOMETRY           QRect(234, 226, 78, 48) //上ボタンの形状
 #define GC_HORIZON_RETURN_GEOMETRY       QRect(390, 226, 78, 48) //戻るボタンの形状
